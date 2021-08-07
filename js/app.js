1bc32b84d8f79b4e68adc1456c779f8ec3daa518
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2021-08-07 15:32:22 -0700

    convert paperscript to javascript

--- js/app.js
+++ js/app.js
@@ -1,44 +1,50 @@
-// Create a raster item using the image tag with id='mona'
-var raster = new Raster('mona');
 
-// Hide the raster:
+// Make the paper scope global by injecting it into window:
+paper.install(window);
+// Setup the paper canvas
+// Get a reference to the canvas object
+let canvas = document.getElementById('myCanvas');
+paper.setup(canvas);
+
+// Define global variables
+let raster = new Raster('mona'); // mona lisa image raster
 raster.visible = false;
+let gridSize = 12; // The size of the grid cells
+let spacing = 1.2; // Space the cells by 120%
+let cells = []; // array to hold the cells
 
-// The size of our grid cells:
-var gridSize = 12;
+// Setup event listeners
+raster.on('load', paintCells); // wait for the raster to load
+// addEventListener("resize", resizeCanvas); // auto-resize canvas
 
-// Space the cells by 120%:
-var spacing = 1.2;
+// Create the drawing
+function paintCells() {
+    raster.size = new Size(40, 30); // reduce number of pixels to loop through
 
-// As the web is asynchronous, we need to wait for the raster to load
-// before we can perform any operation on its pixels.
-raster.on('load', function() {
-    // Since the example image we're using is much too large,
-    // and therefore has way too many pixels, lets downsize it to
-    // 40 pixels wide and 30 pixels high:
-    raster.size = new Size(40, 30);
-
-    for (var y = 0; y < raster.height; y++) {
-        for (var x = 0; x < raster.width; x++) {
+    for (let y = 0; y < raster.height; y++) {
+        for (let x = 0; x < raster.width; x++) {
             // Get the color of the pixel:
-            var color = raster.getPixel(x, y);
+            let color = raster.getPixel(x, y);
 
             // Create a circle shaped path:
-            var path = new Path.Circle({
-                center: new Point(x, y) * gridSize,
+            let path = new Path.Circle({
+                center: new Point(x, y).multiply(gridSize),
                 radius: gridSize / 2 / spacing
             });
-
-            // Set the fill color of the path to the color
-            // of the pixel:
             path.fillColor = color;
+
+            cells.push(path); // save the cell references
         }
     }
+    view.draw();
 
     // Move the active layer to the center of the view, so all 
     // the created paths in it appear centered.
     project.activeLayer.position = view.center;
-});
+}
 
-// Move the active layer to the center of the view:
-project.activeLayer.position = view.center;
+// function resizeCanvas() {
+//     canvas.width = innerWidth; // Set the canvas resolution to fill the page
+//     canvas.height = innerHeight;
+// }
+
